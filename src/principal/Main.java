e2553043ef34cc07d4ac74e3dc229325327b08c3
yovan <yovan@Yovanny>
2025-02-05 19:53:41 +0000

    cargar proyecto

---
+++ src/principal/Main.java
@@ -0,0 +1,61 @@
+package principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/* No borres las lineas de programa que tienes en este proyecto.
+		 * Añade sólo lo que te pide el enunciado de cada ejercicio.
+		*/
+	
+	    // --------------------------------------------------------------------
+		// EJERCICIO 2 (puedes abrir el hueco que necesites) ------------------
+
+
+            // Escribe tu solución AQUI !!!
+
+				
+
+	    // --------------------------------------------------------------------
+		// EJERCICIO 3 (puedes abrir el hueco que necesites) ------------------
+
+		
+            // Escribe tu solución AQUI !!!
+
+		
+		// EJERCICIO 4 (puedes abrir el hueco que necesites) ------------------
+		System.out.println("Comienzo del ejercicio J2.4 --------------------");
+		System.out.println("Contenido del array de numeros:");
+
+
+            // Escribe tu solución AQUI !!!
+		
+
+		
+		// EJERCICIO 5 (puedes abrir el hueco que necesites) ------------------
+		System.out.println("Comienzo del ejercicio J2.5 --------------------");
+		System.out.println("Contenido del array de caracteres:");
+
+            // Escribe tu solución para mostrar el array de caracteres AQUI !!!
+
+
+		System.out.println("Contenido del array de nombres:");
+
+            // Escribe tu solución para mostrar el array de nombres AQUI !!!
+
+
+
+
+		// EJERCICIO 6 (puedes abrir el hueco que necesites) ------------------
+		System.out.println("Comienzo del ejercicio J2.6 --------------------");
+		System.out.println("Nuevo contenido del array de numeros:");
+
+
+            // Escribe tu solución AQUI !!!
+
+		
+		// -------------------------------------------------------------------
+		System.out.println("\nfin del ejercicio J2 ---------------------------");
+	}
+
+}
