51c04c3037357a66029a10e9df067a021ce1f036
yovan <yovan@10.195.39.169>
2025-02-07 13:39:53 +0000

    TODO

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -11,47 +11,73 @@ public class Main {
 	    // --------------------------------------------------------------------
 		// EJERCICIO 2 (puedes abrir el hueco que necesites) ------------------
 
-
-            // Escribe tu solución AQUI !!!
-
+		int[]numeros = new int[5];
+		char[]caracteres = new char[10];
+		String[]nombres = new String[4];
 				
-
 	    // --------------------------------------------------------------------
 		// EJERCICIO 3 (puedes abrir el hueco que necesites) ------------------
-
+	
+		numeros[0] = 1;
+		numeros[1] = 5;
+		numeros[2] = 8;
+		numeros[3] = 12;
+		numeros[4] = 42;
 		
-            // Escribe tu solución AQUI !!!
-
+		caracteres[0] = 'A';
+		caracteres[1] = 'B';
+		caracteres[2] = 'C';
+		caracteres[3] = 'D';
+		caracteres[4] = 'E';
+		caracteres[5] = 'F';
+		caracteres[6] = 'G';
+		caracteres[7] = 'H';
+		caracteres[8] = 'I';
+		caracteres[9] = 'J';
+		
+		nombres[0] = "Carmen";
+		nombres[1] = "Javier";
+		nombres[2] = "Pablo";
+		nombres[3] = "Laura";
+		
 		
 		// EJERCICIO 4 (puedes abrir el hueco que necesites) ------------------
 		System.out.println("Comienzo del ejercicio J2.4 --------------------");
 		System.out.println("Contenido del array de numeros:");
 
 
-            // Escribe tu solución AQUI !!!
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println(numeros[i]);
+		}
 		
 
-		
 		// EJERCICIO 5 (puedes abrir el hueco que necesites) ------------------
 		System.out.println("Comienzo del ejercicio J2.5 --------------------");
 		System.out.println("Contenido del array de caracteres:");
 
-            // Escribe tu solución para mostrar el array de caracteres AQUI !!!
+		for (int k = 0; k < caracteres.length; k++) {
+			System.out.println(caracteres[k]);
+		}
 
 
 		System.out.println("Contenido del array de nombres:");
 
-            // Escribe tu solución para mostrar el array de nombres AQUI !!!
-
-
-
+		for (int l = 0; l < nombres.length; l++) {
+			System.out.println(nombres[l]);
+		}
 
 		// EJERCICIO 6 (puedes abrir el hueco que necesites) ------------------
 		System.out.println("Comienzo del ejercicio J2.6 --------------------");
 		System.out.println("Nuevo contenido del array de numeros:");
 
 
-            // Escribe tu solución AQUI !!!
+        for (int n = 0; n < numeros.length; n++) {
+        	numeros[n] = numeros[n] + 10;
+            System.out.print(numeros[n]);
+            if (n < numeros.length - 1) {
+                System.out.print(",");
+            }
+        }
 
 		
 		// -------------------------------------------------------------------
